90d52179d8542250a6d00f6b7fe9065871223f11
Marcelo Quesada <m.mergulhao@hotmail.com>
2025-03-18 23:00:21 -0300

    Update N-body Planet.cs
    Implementation of simple orbital coordinate system viewing

--- Assets/Scripts/N-body Planet.cs
+++ Assets/Scripts/N-body Planet.cs
@@ -12,9 +12,12 @@ public class N_bodyPlanet : MonoBehaviour
     public double mass;
     public Vector3 initialVelocity;
     public float radius;
+    public float coordSystemScale;
 
     private Vector3 velocity;
     private Transform mesh;
+    private N_bodyPlanet closestBody;
+
 
 
 
@@ -22,6 +25,24 @@ public class N_bodyPlanet : MonoBehaviour
 
     void Awake() { 
         this.velocity = initialVelocity;
+
+        // Find closest body
+        closestBody = null;
+        float closestDistance = float.MaxValue;
+        foreach (N_bodyPlanet body in FindObjectsByType<N_bodyPlanet>(FindObjectsSortMode.None))
+        {
+            if (body == this)
+            {
+                continue;
+            }
+
+            float distance = Vector3.Distance(body.transform.position, transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestBody = body;
+            }
+        }
     }
 
 
@@ -37,5 +58,26 @@ public class N_bodyPlanet : MonoBehaviour
     {
         mesh = transform.GetChild(0);
         mesh.localScale = Vector3.one * radius;
+    }
+
+    public void OnDrawGizmos()
+    {
+        drawCoordinateSystem(coordSystemScale);
+    }
+
+    void drawCoordinateSystem(float scale)
+    {
+        //Draw velocity vector
+        Handles.color = Color.cyan;
+        Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(velocity), velocity.magnitude/(10*scale), EventType.Repaint);
+
+        //Draw XYZ axis
+        Vector3 r = closestBody.gameObject.transform.position - transform.position;
+        Handles.color = Color.red;
+        Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(r), scale, EventType.Repaint);
+        Handles.color = Color.green;
+        Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(velocity), scale, EventType.Repaint);
+        Handles.color = Color.blue;
+        Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(Vector3.Cross(r, velocity)), scale, EventType.Repaint);
     }
 }
