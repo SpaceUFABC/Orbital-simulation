34a2bb7a95b3d0ebcda32cf3b41c4eacc5040116
Marcelo Quesada <m.mergulhao@hotmail.com>
2025-03-18 21:44:22 -0300

    Update NBodySupervisor.cs

--- Assets/Scripts/NBodySupervisor.cs
+++ Assets/Scripts/NBodySupervisor.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 
+
 public class NBodySupervisor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    N_bodyPlanet[] planets;
+    public float timeStep = 0.01f;
+
+    void Awake()
     {
-        
+        // Find all planet objects
+        planets = FindObjectsByType<N_bodyPlanet>(FindObjectsSortMode.None);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        foreach (N_bodyPlanet planet in planets)
+        {
+            planet.updateVelocity(timeStep);
+        }
+
+        foreach (N_bodyPlanet planet in planets)
+        {
+            planet.updatePosition(timeStep);
+        }
     }
 }
